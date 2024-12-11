699322ec0c57f8099fa6c6f4a9cc35a8f527f6fe
bridgezan <bridgezan@gmail.com>
2024-12-11 14:08:03 +0330

    Update coderrbw-ss.ps1

--- coderrbw-ss.ps1
+++ coderrbw-ss.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "SilentlyContinue"
 
-if (-not ([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) { Start-Process -FilePath "powershell" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 function Get-Signature {
 
     [CmdletBinding()]
